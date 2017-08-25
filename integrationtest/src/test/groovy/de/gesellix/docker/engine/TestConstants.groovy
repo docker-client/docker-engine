4d1a75f0011c6408d01a1cc9d4b50120b1f61753
Tobias Gesellchen <tobias@gesellix.de>
2017-08-25 09:09:40 +0200

    update golden test data

--- integrationtest/src/test/groovy/de/gesellix/docker/engine/TestConstants.groovy
+++ integrationtest/src/test/groovy/de/gesellix/docker/engine/TestConstants.groovy
@@ -51,13 +51,13 @@ class TestConstants {
             versionDetails = [
                     ApiVersion   : { it == "1.31" },
                     Arch         : { it == "amd64" },
-                    BuildTime    : { it == "2017-08-17T01:32:35.653311639+00:00" },
-                    GitCommit    : { it == "665d244" },
+                    BuildTime    : { it == "2017-08-22T22:30:35.498879141+00:00" },
+                    GitCommit    : { it == "fe143e3" },
                     GoVersion    : { it == "go1.8.3" },
                     KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}(-\\w+)?" },
                     MinAPIVersion: { it == "1.12" },
                     Os           : { it == "linux" },
-                    Version      : { it == "17.07.0-ce-rc3" }]
+                    Version      : { it == "17.07.0-ce-rc4" }]
         }
     }
 }
