077ca96a1a1502fd2678c53f500239e2c7dc1051
Tobias Gesellchen <tobias@gesellix.de>
2020-02-29 19:15:01 +0100

    Update dependencies

--- integrationtest/build.gradle.kts
+++ integrationtest/build.gradle.kts
@@ -26,12 +26,11 @@ repositories {
 dependencies {
     implementation(project(":engine"))
 
-    testImplementation("org.slf4j:slf4j-api:1.7.29")
+    testImplementation("org.slf4j:slf4j-api:1.7.30")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
 
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
     testImplementation("cglib:cglib-nodep:3.3.0")
-    testImplementation("joda-time:joda-time:2.10.5")
 }
 tasks.check.get().shouldRunAfter(project(":engine").tasks.check)
 
