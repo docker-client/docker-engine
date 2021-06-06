2aa7e29d08d1996e09aaab99fa450104731ad3a1
Tobias Gesellchen <tobias@gesellix.de>
2021-06-06 22:31:22 +0200

    Add api-model as subproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,24 @@ plugins {
   id("net.ossindex.audit") version "0.4.11"
   id("io.freefair.maven-central.validate-poms") version "5.3.3.3"
   id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+  id("org.jetbrains.kotlin.jvm") version "1.4.31" apply false
+  id("org.jetbrains.kotlin.kapt") version "1.4.31" apply false
 }
 
 val dependencyVersions = listOf(
-  "org.jetbrains:annotations:21.0.1"
+  "com.squareup.okio:okio:2.10.0",
+  "org.jetbrains:annotations:21.0.1",
+  "org.jetbrains.kotlin:kotlin-reflect:1.4.31",
+  "org.jetbrains.kotlin:kotlin-stdlib:1.4.31",
+  "org.jetbrains.kotlin:kotlin-stdlib-common:1.4.31",
+  "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.31",
+  "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.31"
 )
 
-val dependencyGroupVersions = mapOf<String, String>()
+val dependencyGroupVersions = mapOf(
+  "org.junit.jupiter" to "5.7.2",
+  "org.junit.platform" to "1.7.2"
+)
 
 subprojects {
   configurations.all {
