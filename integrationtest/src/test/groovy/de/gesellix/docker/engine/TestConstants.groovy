903bdf0a10209516ef3cec032d0e6f1b23dfaad4
Tobias Gesellchen <tobias@gesellix.de>
2023-12-19 10:45:12 +0100

    Bump echo-server, cleanup test constants

--- integrationtest/src/test/groovy/de/gesellix/docker/engine/TestConstants.groovy
+++ integrationtest/src/test/groovy/de/gesellix/docker/engine/TestConstants.groovy
@@ -12,59 +12,22 @@ class TestConstants {
 
   TestConstants() {
     imageRepo = "gesellix/echo-server"
-    imageTag = "2023-07-02T12-00-00"
+    imageTag = "2023-12-19T09-50-00"
     imageName = "$imageRepo:$imageTag"
 
-    if (System.getenv("GITHUB_ACTOR")) {
-      // TODO consider checking the Docker api version instead of "GITHUB_ACTOR"
-      if (LocalDocker.isNativeWindows()) {
-        versionDetails = [
-            ApiVersion   : { it == "1.43" },
-            Arch         : { it in ["amd64", "arm64"] },
-            BuildTime    : { it =~ "2023-\\d{2}-\\d{2}T\\w+" },
-            GitCommit    : { it =~ "\\w{6,}" },
-            GoVersion    : { it =~ "go\\d+.\\d+.\\d+" },
-            KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}\\w*" },
-            MinAPIVersion: { it == "1.24" },
-            Os           : { it == "windows" },
-            Version      : { it =~ "\\d{1,2}.\\d{1,2}.\\d{1,2}\\w*" }]
-      }
-      else {
-        versionDetails = [
-            ApiVersion   : { it == "1.43" },
-            Arch         : { it in ["amd64", "arm64"] },
-            BuildTime    : { it =~ "2023-\\d{2}-\\d{2}T\\w+" },
-            GitCommit    : { it =~ "\\w{6,}" },
-            GoVersion    : { it =~ "go\\d+.\\d+.\\d+" },
-            KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}\\w*" },
-            MinAPIVersion: { it == "1.12" },
-            Os           : { it == "linux" },
-            Version      : { it =~ "\\d{1,2}.\\d{1,2}.\\d{1,2}\\w*" }]
-      }
-    }
-    else if (LocalDocker.isNativeWindows()) {
-      versionDetails = [
-          ApiVersion   : { it == "1.43" },
-          Arch         : { it in ["amd64", "arm64"] },
-          BuildTime    : { it =~ "2023-05-25T\\w+" },
-          GitCommit    : { it == "659604f9" },
-          GoVersion    : { it =~ "go\\d+.\\d+.\\d+" },
-          KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}\\w*" },
-          MinAPIVersion: { it == "1.24" },
-          Os           : { it == "windows" },
-          Version      : { it =~ "\\d{1,2}.\\d{1,2}.\\d{1,2}\\w*" }]
-    }
-    else {
-      versionDetails = [
-          ApiVersion   : { it == "1.44" },
-          Arch         : { it in ["amd64", "arm64"] },
-          BuildTime    : { it =~ "2023-08-31T\\w+" },
-          GitCommit    : { it == "8dfaf0c780630c4f3a6ee51b324bdf50b36e2bb3" },
-          GoVersion    : { it =~ "go\\d+.\\d+.\\d+" },
-          KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}\\w*" },
-          MinAPIVersion: { it == "1.12" },
-          Os           : { it == "linux" },
-          Version      : { it =~ "\\d{1,2}.\\d{1,2}.\\d{1,2}\\w*" }]
+    versionDetails = [
+        ApiVersion   : { it in ["1.43", "1.44"] },
+        Arch         : { it in ["amd64", "arm64"] },
+        BuildTime    : { it =~ "\\d{4}-\\d{2}-\\d{2}T\\w+" },
+        GitCommit    : { it =~ "\\w{6,}" },
+        GoVersion    : { it =~ "go\\d+.\\d+.\\d+" },
+        KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}\\w*" },
+        MinAPIVersion: { it == "1.12" },
+        Os           : { it == "linux" },
+        Version      : { it == "master" || it =~ "\\d{1,2}.\\d{1,2}.\\d{1,2}\\w*" }]
+    if (LocalDocker.isNativeWindows()) {
+      versionDetails.MinAPIVersion = { it == "1.24" }
+      versionDetails.Os = { it == "windows" }
     }
   }
 }
