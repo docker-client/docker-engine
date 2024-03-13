74f0ed037866c7b247c1bcb4f32730421ed9d9fd
Tobias Gesellchen <tobias@gesellix.de>
2024-03-13 22:10:46 +0100

    Remove the explicit dependency on commons-codec after the upstream fix in commons-compress

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -77,8 +77,6 @@ dependencies {
   testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 
   implementation("org.apache.commons:commons-compress:1.26.1")
-  // declared as optional by commons-compress-1.26, but we need it for org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
-  implementation("commons-codec:commons-codec:1.16.1")
   testImplementation("org.apache.commons:commons-lang3:3.14.0")
 
   implementation("de.gesellix:docker-filesocket:2024-02-21T19-20-00")
