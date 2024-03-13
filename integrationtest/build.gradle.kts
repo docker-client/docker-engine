a9c66bab47550239272dc2e3a99907a1a05da5a6
Tobias Gesellchen <tobias@gesellix.de>
2024-03-13 21:52:58 +0100

    Prefer okio 3.9.0

--- integrationtest/build.gradle.kts
+++ integrationtest/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("com.squareup.okio:okio") {
       version {
         strictly("[3,4)")
-        prefer("3.8.0")
+        prefer("3.9.0")
       }
     }
     listOf(
