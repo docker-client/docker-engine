55b491c387c5e67f5ad55e597a499977058e83cf
Tobias Gesellchen <tobias@gesellix.de>
2024-06-09 21:04:11 +0200

    Bump supported API version check

--- integrationtest/src/test/groovy/de/gesellix/docker/engine/TestConstants.groovy
+++ integrationtest/src/test/groovy/de/gesellix/docker/engine/TestConstants.groovy
@@ -16,17 +16,16 @@ class TestConstants {
     imageName = "$imageRepo:$imageTag"
 
     versionDetails = [
-        ApiVersion   : { it in ["1.43", "1.44"] },
+        ApiVersion   : { it in ["1.43", "1.44", "1.45"] },
         Arch         : { it in ["amd64", "arm64"] },
         BuildTime    : { it =~ "\\d{4}-\\d{2}-\\d{2}T\\w+" },
         GitCommit    : { it =~ "\\w{6,}" },
         GoVersion    : { it =~ "go\\d+.\\d+.\\d+" },
         KernelVersion: { it =~ "\\d.\\d{1,2}.\\d{1,2}\\w*" },
-        MinAPIVersion: { it == "1.12" },
+        MinAPIVersion: { it in ["1.12", "1.24"] },
         Os           : { it == "linux" },
         Version      : { it == "master" || it =~ "\\d{1,2}.\\d{1,2}.\\d{1,2}\\w*" }]
     if (LocalDocker.isNativeWindows()) {
-      versionDetails.MinAPIVersion = { it == "1.24" }
       versionDetails.Os = { it == "windows" }
     }
   }
